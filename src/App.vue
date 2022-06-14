bc7f108f5f76ec207cfcaaa5013a482645093cce
monkeymr014 <monkeymr014@gmail.com>
2022-06-14 14:27:08 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,7 @@
 <template>
   <div id="app">
     <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">Home</router-link>
     </nav>
     <router-view/>
   </div>
@@ -14,7 +13,8 @@
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #fffdff;
+  background: #263e31;
 }
 
 nav {
---
+++ src/components/Creator.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="full">
+    <h2>Kreator tapicerki</h2>
+    <div class="row">
+      <div class="col-4">
+        
+      </div>
+
+
+      <div class="col-8">col-4</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+};
+</script>
+<style lang="css" scoped>
+
+  div {
+    border: solid red;
+  }
+
+</style>
+
+
